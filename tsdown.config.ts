3b0df61dfca81e3c485657515129ca80439a1434
hoah2333 <c3n6fak@gmail.com>
2026-07-16 02:43:20 +0800

    chore: 更新依赖；prettier

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  exports: true,
-  dts: true,
-})
+export default defineConfig({ entry: ["src/index.ts"], format: ["esm", "cjs"], exports: true, dts: true });
